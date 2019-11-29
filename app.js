a5b7959f290edccc3ec49610b5cef4046a6ca2af
Johan <johan225@hotmail.es>
2019-11-29 15:24:41 -0500

    Soluciona

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-  res.send(req.query.nombre ? "<h1>Hola " + req.query.nombre + "!</h1>" : "<h1>Hola desconocido!</h1>");
+app.get('/makers/:name', (req, res) => {
+  const name = req.params.name
+  const nameCap =  name.charAt(0).toUpperCase() + name.slice(1)
+  res.send("<h1>Hola " + nameCap + "!</h1>");
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
